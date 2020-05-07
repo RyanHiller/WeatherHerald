6a1c34037b83b16c9f8fd706ac60c4d4b6b4af47
Ryan Hiller <allamericanslacker@gmail.com>
2020-05-07 09:34:36 -0600

    Home page temperature rendering correctly, temp conversion not.

--- app/src/main/java/msudenver/cs3013/spring2020/weatherherald/HomeFragment.kt
+++ app/src/main/java/msudenver/cs3013/spring2020/weatherherald/HomeFragment.kt
@@ -1,6 +1,7 @@
 package msudenver.cs3013.spring2020.weatherherald
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,12 +14,33 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val tempView = activity?.findViewById<TextView>(R.id.current_temp)
-        val temperature = PreferenceManager.getDefaultSharedPreferences(activity?.baseContext)
-            .getFloat("current_temp", 0.0F)
-        tempView?.text = temperature.toString()
+        val view = inflater.inflate(R.layout.fragment_home2, container, false)
+
+        updateUI(view)
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+
+    private fun updateUI(view: View) {
+        val currTempView = view.findViewById<TextView>(R.id.currentTemp)
+        val dayHighView = view.findViewById<TextView>(R.id.dayHigh)
+        val dayLowView = view.findViewById<TextView>(R.id.dayLow)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity?.baseContext)
+        val currTemp = sharedPreferences.getFloat("current_temp", 0.0F)
+        val dayHigh = sharedPreferences.getFloat("today_high", 0.0F)
+        val dayLow = sharedPreferences.getFloat("today_low", 0.0F)
+        val tempUnit = if (PreferenceManager.getDefaultSharedPreferences(activity?.baseContext)
+                .getBoolean("temp_toggle", false)
+        ) "C" else "F"
+        val degreeSign = getText(R.string.degreeSign)
+
+        val currTempString = "$currTemp${degreeSign}${tempUnit}"
+        val dayHighString = "$dayHigh${degreeSign}${tempUnit}"
+        val dayLowString = "$dayLow${degreeSign}${tempUnit}"
+
+        currTempView?.text = currTempString
+        dayHighView?.text = dayHighString
+        dayLowView?.text = dayLowString
     }
 }
